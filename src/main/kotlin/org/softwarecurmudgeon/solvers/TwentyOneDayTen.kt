4237c378043f7b5e814238ecaab8a8c685c46b30
dix <wdix@gofundme.com>
2021-12-10 07:15:43 -0700

    2021 - refactor parens traversal

--- src/main/kotlin/org/softwarecurmudgeon/solvers/TwentyOneDayTen.kt
+++ src/main/kotlin/org/softwarecurmudgeon/solvers/TwentyOneDayTen.kt
@@ -26,6 +26,16 @@ object TwentyOneDayTen: Solution<List<Char>, Long>(), Solver {
             }
             .sum()
 
+    override fun partTwo(input: Sequence<List<Char>>): Long {
+        val scores = input
+            .map(::findCompletion)
+            .filter(List<Char>::isNotEmpty)
+            .map(::scoreCompletion)
+            .sortedDescending()
+            .toList()
+        return scores[scores.count() / 2]
+    }
+
     private val openers = setOf(
         '[',
         '(',
@@ -45,21 +55,6 @@ object TwentyOneDayTen: Solution<List<Char>, Long>(), Solver {
     fun corrupt(line: List<Char>): Boolean =
         !validate(line)
 
-    tailrec fun firstCorrupt(remaining: List<Char>, searching: List<Char> = listOf()): Char? =
-        if (remaining.isEmpty() && searching.isEmpty()) {
-            null
-        }  else if (remaining.isEmpty() && searching.isNotEmpty())
-            null
-        else {
-            val first = remaining.first()
-            val rest = remaining.drop(1)
-            when (first) {
-                in openers -> firstCorrupt(rest, searching.plus(closerForOpener(first)))
-                searching.last() -> firstCorrupt(rest, searching.dropLast(1))
-                else -> first
-            }
-        }
-
     fun scoreCompletion(input: List<Char>): Long =
         input.fold(0) { score, next ->
             (score * 5) + when(next) {
@@ -71,47 +66,63 @@ object TwentyOneDayTen: Solution<List<Char>, Long>(), Solver {
             }
         }
 
-    tailrec fun findCompletion(remaining: List<Char>, searching: List<Char> = listOf()): List<Char> =
-        if (remaining.isEmpty() && searching.isEmpty()) {
-            listOf()
-        }  else if (remaining.isEmpty() && searching.isNotEmpty())
-            searching.reversed()
-        else {
-            val first = remaining.first()
-            val rest = remaining.drop(1)
-            when (first) {
-                in openers -> findCompletion(rest, searching.plus(closerForOpener(first)))
-                searching.last() -> findCompletion(rest, searching.dropLast(1))
-                else -> listOf()
-            }
-        }
+    fun firstCorrupt(remaining: List<Char>): Char? =
+        navigateParens(
+            remaining,
+            listOf(),
+            null,
+            { null },
+            { it }
+        )
 
-    private tailrec fun validate(remaining: List<Char>, searching: List<Char> = listOf()): Boolean =
-        if (remaining.isEmpty() && searching.isEmpty()) {
-            true
-        }  else if (remaining.isEmpty() && searching.isNotEmpty())
-            false
-        else {
-            val first = remaining.first()
-            val rest = remaining.drop(1)
-            when (first) {
-                in openers -> validate(rest, searching.plus(first))
-                searching.first() -> validate(rest, searching.drop(1))
-                else -> false
-            }
-        }
+    fun findCompletion(remaining: List<Char>, searching: List<Char> = listOf()): List<Char> =
+        navigateParens(
+            remaining,
+            searching,
+            listOf(),
+            { it.reversed() },
+            { listOf() }
+        )
 
-    override fun partTwo(input: Sequence<List<Char>>): Long {
-        val scores = input
-            .map(::findCompletion)
-            .filter(List<Char>::isNotEmpty)
-            .map(::scoreCompletion)
-            .sortedDescending()
-            .toList()
-        println(scores.count())
-        println( scores[(scores.count() / 2) + 1])
-        println( scores[(scores.count() / 2) - 1])
-        println( scores[(scores.count() / 2)])
-        return scores[scores.count() / 2]
+    private fun validate(remaining: List<Char>) =
+        navigateParens(
+            remaining,
+            listOf(),
+            true,
+            { false },
+            { false }
+        )
+
+    private tailrec fun <T> navigateParens(
+        remaining: List<Char>,
+        searching: List<Char> = listOf(),
+        emptyCase: T,
+        emptyRemainingCase: (List<Char>) -> T,
+        mismatchCase: (Char) -> T
+    ): T = if (remaining.isEmpty() && searching.isEmpty()) {
+        emptyCase
+    }  else if (remaining.isEmpty() && searching.isNotEmpty())
+        emptyRemainingCase(searching)
+    else {
+        val first = remaining.first()
+        val rest = remaining.drop(1)
+        when (first) {
+            in openers -> navigateParens(
+                rest,
+                searching.plus(closerForOpener(first)),
+                emptyCase,
+                emptyRemainingCase,
+                mismatchCase
+            )
+            searching.last() -> navigateParens(
+                rest,
+                searching.dropLast(1),
+                emptyCase,
+                emptyRemainingCase,
+                mismatchCase
+            )
+            else -> mismatchCase(first)
+        }
     }
+
 }
