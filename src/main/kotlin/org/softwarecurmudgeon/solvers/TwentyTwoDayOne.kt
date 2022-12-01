11261ade24b6d981b92a932084039f5e32dcf477
dix <wdix@gofundme.com>
2022-12-01 16:29:31 -0700

    Refactor to use blank line separated sequence

--- src/main/kotlin/org/softwarecurmudgeon/solvers/TwentyTwoDayOne.kt
+++ src/main/kotlin/org/softwarecurmudgeon/solvers/TwentyTwoDayOne.kt
@@ -1,36 +1,22 @@
 package org.softwarecurmudgeon.solvers
 
+import org.softwarecurmudgeon.common.BlankLineSeparatedSequence
 import org.softwarecurmudgeon.common.Day
 
-object TwentyTwoDayOne : Solution<Int?, Int>(), Solver {
+object TwentyTwoDayOne : Solution<Sequence<Int>, Int>(), Solver {
     override val day: Day
         get() = Day(year = 2022, day = 1)
 
-    override fun parseInput(input: Sequence<String>): Sequence<Int?> {
-        return input.map {
-            it.toIntOrNull()
-        }
+    override fun parseInput(input: Sequence<String>): Sequence<Sequence<Int>> {
+        return BlankLineSeparatedSequence.generate(input)
+            .map { it.mapNotNull(String::toIntOrNull) }
     }
 
-    override fun partTwo(input: Sequence<Int?>): Int {
-        return input.fold(Pair(emptyList<Int>(), 0)) { (sums, current), element ->
-            if (element == null) {
-                Pair(sums + current, 0)
-            } else {
-                Pair(sums, current + element)
-            }
-        }.let { (sums, current) ->
-            sums.plus(current).sortedDescending().take(n = 3).sum()
-        }
+    override fun partTwo(input: Sequence<Sequence<Int>>): Int {
+        return input.map { it.sum() }.sortedDescending().take(n = 3).sum()
     }
 
-    override fun partOne(input: Sequence<Int?>): Int {
-        return input.fold(Pair(0, 0)) {(max, current), element ->
-            if (element == null) {
-                Pair(max.coerceAtLeast(current), 0)
-            } else {
-                Pair(max, current + element)
-            }
-        }.first
+    override fun partOne(input: Sequence<Sequence<Int>>): Int {
+        return input.map{ it.sum() }.maxOrNull()!!
     }
 }
